6c5e1aef4832cdd8cdfbe7c493e19428113c80ed
kevinharr <kevinharr@verizon.net>
2023-01-04 09:55:52 -0500

    complete lab

---
+++ data/students.js
@@ -0,0 +1,9 @@
+const students = [
+    {text: 'Kevin Harrington', done: false, _id: 123444},
+    {text: 'Jane Doe', done: true, _id: 123445},
+    {text: 'Mike Roman', done: true, _id: 123446},
+  ]
+  
+  export { 
+      students
+  }
